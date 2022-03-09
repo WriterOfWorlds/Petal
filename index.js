4c0cdd0735f565ae50b73dc7705ffc6f1bb65b82
TheLoan <57270802+WriterOfWorlds@users.noreply.github.com>
2022-03-09 20:44:36 +0000

    A lot, actual cryptography functions, now hosting through Express, should hook up to a domain soon enough.

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const pbkdf2 = require('pbkdf2');  
-const fs = require('fs');
-const piexif = require('piexifjs');
-const Jimp = require("jimp");
-const cheese = require("./ethanscheese")
-const getBase64DataFromJpegFile = filename => fs.readFileSync(filename).toString('binary');
-const getExifFromJpegFile = filename => piexif.load(getBase64DataFromJpegFile(filename));
+const { SHA3 } = require('sha3');
+const pbkdf2 = require('pbkdf2')
 
-// Read the PNG file and convert it to editable format
-Jimp.read("./static.png", function (err, image) {
-  if (err) {
-      
-    // Return if any error
-    console.log(err);
-    return;
+const path = require('path');
+const express = require('express');
+	 
+const app = express();
+const router = express.Router();
+
+function generateSHA3(optional) {
+  // Generate a SHA3 hash
+  const hash = new SHA3(512);
+
+  if (optional != null) {
+    hash.update(optional);
+  } else {
+    // Update it with the number of milliseconds elapsed since the Unix Epoch (Makes every hash unique)
+    hash.update(String(Date.now()));
   }
-  
-  // Convert image to JPG and store it to 
-  // './output/' folder with 'out.jpg' name
-  image.write("./output/out.jpg");
-});
 
-const scrubbedStatic = piexif.remove(getBase64DataFromJpegFile("./output/out.jpg"))
-fileBuffer = Buffer.from(scrubbedStatic, 'binary');
-fs.writeFileSync('./output/out2.jpg', fileBuffer);
+  // Output the digested hash as a hex string
+  return(hash.digest('hex'));
+}
+
+function generateDerivedKey(password) {
+  return(pbkdf2.pbkdf2Sync(String(password), generateSHA3(String(password)), 4, 32, 'sha512'))
+}
+
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.use('/', router);
+
+let server = app.listen(3000, function(){});
